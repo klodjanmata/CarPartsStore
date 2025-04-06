32a42be1a3113b22f567ea5447a807c5c70f7321
orgen080 <hamatiorgen@gmail.com>
2025-04-06 10:38:42 +0200

    hibernate config git remote add origin https://github.com/klodjanmata/CarPartsStore.git git branch -M main git push -u origin main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import Util.Helper;
+
 public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static int getChoice(){
+        int choice = -1;
+        try{
+            choice = Helper.getIntFromUser();
+        }catch (Exception e){
+            choice = -1;
+        }
+        return choice;
+    }
 }
+
